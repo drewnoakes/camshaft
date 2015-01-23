4447adb2e8bc86a2f493f5888ec9e8f713f57fcb
Drew Noakes <git@drewnoakes.com>
2015-01-23 10:47:43 +0000

    JsonHandler sends error responses.

--- include/camshaft/lwsxx/jsonhandler.hh
+++ include/camshaft/lwsxx/jsonhandler.hh
@@ -4,36 +4,21 @@
 #include <string>
 #include <vector>
 
-#include <rapidjson/document.h>
-#include <rapidjson/error/en.h>
-
 #include <lwsxx/websockethandler.hh>
-#include <lwsxx/websocketsession.hh>
+
+#include <rapidjson/document.h>
+
+namespace lwsxx { class WebSocketSession; }
 
 namespace camshaft
 {
   class JsonHandler : public lwsxx::WebSocketHandler
   {
   protected:
-    void receiveMessage(lwsxx::WebSocketSession* session, std::vector<byte>& message) override
-    {
-      message.push_back('\0');
-
-      rapidjson::Document doc;
-      doc.ParseInsitu(reinterpret_cast<char*>(message.data()));
-
-      if (doc.HasParseError())
-        onJsonParseError(session, doc);
-      else
-        onDocument(session, doc);
-    }
+    void receiveMessage(lwsxx::WebSocketSession* session, std::vector<byte>& message) override;
 
     virtual void onDocument(lwsxx::WebSocketSession* session, const rapidjson::Document& doc) = 0;
 
-    virtual void onJsonParseError(lwsxx::WebSocketSession* session, const rapidjson::Document& doc)
-    {
-      std::cerr << "Error parsing command JSON message at offset " << doc.GetErrorOffset()
-        << ": " << GetParseError_En(doc.GetParseError()) << std::endl;
-    };
+    virtual void onJsonParseError(lwsxx::WebSocketSession* session, const rapidjson::Document& doc);
   };
 }
